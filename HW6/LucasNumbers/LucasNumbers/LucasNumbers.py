eafc06b88b129e9ab31685b4216d32d363cdefdb
dkStephanos <stephanos@goldmail.etsu.edu>
2020-04-04 18:57:50 -0400

    Refactor

--- HW6/LucasNumbers/LucasNumbers/LucasNumbers.py
+++ HW6/LucasNumbers/LucasNumbers/LucasNumbers.py
@@ -9,7 +9,7 @@ def getParams():
 
     return [lucasIndex, timeSpan]
 
-if __name__ == "__main__":
+def problemOneDriver():
     problemOne = ProblemOne()
     
     while True:
@@ -22,10 +22,13 @@ if __name__ == "__main__":
             try:
                startTime = time.time()
                endTime = startTime + timeSpan
-               result = problemOne.calcLucasNumber(currIndex, startTime, endTime)
-               print('lucas {0} is {1} - computed with {2} calls in {3} seconds'.format(result[0], result[1], result[2], (timeSpan - (endTime - time.time()))))
+               result = problemOne.calcLucasNumber(currIndex, startTime, timeSpan)
+               print('lucas {0} is {1} - computed with {2} calls in {3} seconds'.format(currIndex, result[0], result[1], (timeSpan - (endTime - time.time()))))
                currIndex += 1
                endTime = time.time() + timeSpan
             except TimeoutError as err:
                 print('\n\ntimeout at lucas {0} after {1} {2}'.format(currIndex, timeSpan, 'seconds' if timeSpan > 1 else 'second'))
                 break
+
+if __name__ == "__main__":
+    problemOneDriver()
--- HW6/LucasNumbers/LucasNumbers/ProblemOne.py
+++ HW6/LucasNumbers/LucasNumbers/ProblemOne.py
@@ -11,32 +11,32 @@ class ProblemOne(object):
         self.endTime = 0
 
 
-    def calcLucasNumber(self, lucasIndex, startTime, endTime):
-        self.timeSpan = endTime - startTime
-        self.endTime = endTime
+    def calcLucasNumber(self, lucasIndex, startTime, timeSpan):
+        self.timeSpan = timeSpan
+        self.endTime = startTime + timeSpan
         #If lucasIndex or timeSpan requested is negative, return -1 to indicate invalid params
-        if(lucasIndex < 0 or self.timeSpan < 0):
+        if(lucasIndex < 0 or timeSpan < 0):
             return -1
         
         def getNextNumber(currIndex, endTime):
+            self.numCalls += 1
             if time.time() > endTime:
                 raise TimeoutError("Exceeded timespan")
             elif currIndex < 0:
                 print('\nInvalid index')
             elif currIndex == 0:
-                result =  [0, self.lucasZero]
+                result =  [self.lucasZero, 1]
             elif currIndex == 1:
-                result =  [1, self.lucasOne]
+                result =  [self.lucasOne, 1]
             else:
-                currLucasNum = (getNextNumber(currIndex - 2, endTime)[1] if currIndex -2 > 0 else 2) + (getNextNumber(currIndex - 1, endTime)[1] if currIndex - 1 > 1 else 1)
-                result = [currIndex, currLucasNum]
-            
-            self.numCalls += 1
+                result1 = getNextNumber(currIndex - 1, endTime)
+                result2 = getNextNumber(currIndex - 2, endTime)
+                result = [result1[0] + result2[0], result1[1] + result2[1] + 1]
+
             return result
         
         result = getNextNumber(lucasIndex, self.endTime)
         #Reset self.numCalls so I can call method multiple times in one session
-        numCalls = self.numCalls
         self.numCalls = 0
-        #Returns [currIndex, currLucasNum, numCalls]
-        return [result[0], result[1], numCalls]
+        #Returns [currLucasNum, numCalls]
+        return result
